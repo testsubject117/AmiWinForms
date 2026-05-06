792b38268c8a469c730205a34947fa9489e7ad01
CapnKirk <capnkirk@capnkirk.com>
2026-05-06 13:31:07 -0700

    Fix Sales Employee checks menu (A) label to show '&' and capitalize Customers

--- AMiOffice/FormSalesEmployeesChecksMenu.vb
+++ AMiOffice/FormSalesEmployeesChecksMenu.vb
@@ -30,11 +30,11 @@ Public Class FormSalesEmployeesChecksMenu
         SetMenuTitle("Sales Employee's checks")
 
         ' (A) Open EMPNAME.DAT editor (DOS opened EDIT)
-        AddMenuButton(flpLeft, "A", "Modify/View Sales Employee List & their customers", Sub()
-                                                                                             Using f As New FormEmpNameEditor()
-                                                                                                 f.ShowDialog(Me)
-                                                                                             End Using
-                                                                                         End Sub)
+        AddMenuButton(flpLeft, "A", "Modify/View Sales Employee List && their customers", Sub()
+                                                                                              Using f As New FormEmpNameEditor()
+                                                                                                  f.ShowDialog(Me)
+                                                                                              End Using
+                                                                                          End Sub)
 
         ' (B) Return to the already-open CHECKS menu (don't open another one)
         AddMenuButton(flpLeft, "B", "Make a check for an Employee.", Sub()
